7986ad90015e310d22472be708c1c32890f98b8a
MSI\hakan <hakansrc@msn.com>
2018-01-22 15:48:13 +0300

    pwm trials

---
+++ mcu/version1/version1.ino
@@ -0,0 +1,52 @@
+int pwm_value = 0;
+int percentage = 0;
+void setup() {
+  pinMode(9,OUTPUT);
+  digitalWrite(9,LOW);
+  setPwmFrequency(9,8);
+  Serial.begin(115200);
+
+}
+
+void loop() {
+//  for(int i=0,;i>255
+
+  //delay(500);
+  pwm_value = analogRead(A0);
+  percentage = map(pwm_value,0,1023,38,215);
+  Serial.print(percentage); 
+  Serial.print('\n');
+ // delay(100);
+  analogWrite(9,percentage);
+
+}
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+        TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x7; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
--- simulation results/calc.m
+++ simulation results/calc.m
@@ -1,13 +1,12 @@
 fs =2500;
 Ts = 1/fs;
 Vd =280;
-L = 10e-6;
+L = 2.9e-3;
 
+% ILmax = Ts*Vd/(8*L);
+% D = 0:0.01:1;
+% IL = ILmax*D.*(1-D);
+% plot(D,IL);
 
-ILmax = Ts*Vd/(8*L);
-D = 0:0.01:1;
-IL = ILmax*D.*(1-D);
-plot(D,IL);
-
-C = 150e-6;
+C = 680e-6;
 fc = 1/(2*pi*sqrt(L*C))
