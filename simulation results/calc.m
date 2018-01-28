d94f2d4cb979e19bb02f3ce5f9a1f9bfbded045d
MSI\hakan <hakansrc@msn.com>
2018-01-28 04:00:05 +0300

    moving on

--- simulation results/calc.m
+++ simulation results/calc.m
@@ -1,12 +1,16 @@
-fs =2500;
+fs =31250;
 Ts = 1/fs;
-Vd =280;
-L = 2.9e-3;
+Vd =220;
+L = 2e-3;
 
-% ILmax = Ts*Vd/(8*L);
-% D = 0:0.01:1;
-% IL = ILmax*D.*(1-D);
-% plot(D,IL);
+ILmax = Ts*Vd/(8*L);
+D = 0:0.01:1;
+IL = ILmax*D.*(1-D);
+figure;
+plot(D,IL);
+title('Boundary between DCM and CCM');
+xlabel('Duty Cycle');
+ylabel('IL (A)');
 
 C = 680e-6;
 fc = 1/(2*pi*sqrt(L*C))
